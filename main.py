5d69ec94086d4a5541e6c58ba0769b74b8d9acf0
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-11-02 12:19:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ def mash_line(arg, arg1):
     import pandas as pd
     from sklearn import linear_model as lm
     import numpy as np
+
     df = pd.read_csv('city.csv')
-    reg = lm.LinearRegression()
     df1 = df[(df['Country'] == arg)]
     df1 = df1.drop('City', 1)
     df1 = df1.drop('Region', 1)
     df1 = df1.drop('Country', 1)
     df2 = df1.sum() / df1.where(df1 > 0).count()
-    df3 = df2.index.tolist()
     df4 = []
-    for i in df3:
-        mass = []
-        for j in i:
-            if j.isdigit():
-                mass.append(j)
+    for i in df2.index.tolist():
+        mass = [j for j in i if j.isdigit()]
         df4.append(int(''.join(mass)))
+
+    reg = lm.LinearRegression()
     reg.fit(np.array(df4).reshape(-1, 1), df2)
-    print('\033[1m''Данные по городам', arg, ',где есть интернет:''\033[0m')
+
+    print('\033[1m''Данные по городам', arg, ',которыми мы обладаем:''\033[0m')
     count = 0
-    for i in df1.sum() / df1.where(df1 > 0).count():
-        print('\033[91m',2010 + count,'\033[0m'' - ', i)
+    for i in df2:
+        print('\033[91m',2010 + count,'\033[0m'' - ', i, 'USD')
         count += 1
     print('\033[1m''В', arg, ',в''\033[91m', arg1,'\033[0m''\033[1m' 'году, интернет будет стоить - ', reg.predict([[arg1]])[0], 'USD''\033[0m')
 
-mash_line('Russia', 2045)
+mash_line('Norway', 2050)
